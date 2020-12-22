1b5949e218fe6df3cdcaad0ad560a4abd28eaed8
whw <1027281456@qq.com>
2020-12-22 17:03:42 +0800

    坐标跑通

--- src/main/resources/static/js/app/map/map.js
+++ src/main/resources/static/js/app/map/map.js
@@ -15,24 +15,29 @@ $(function () {
     map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
 
     var pointlist={"code":200,"data":{"rows":[
-                {xy:"116.404,39.015",title:"测试a",addr:"北京市王府井大街",province:"北京"},{xy:"116.900,39.015",title:"测试a",addr:"北京市王府井大街",province:"北京"},
-            ]}}
+                {x:116.404,y:39.015,title:"测试a",addr:"北京市王府井大街",province:"北京"},{x:116.900,y:39.015,title:"测试b",addr:"北京市王府井大街",province:"北京"},
+            ]}};
+    var marker=new Array;
+    var point=new Array;
+    var infoWindow=new Array;
     for(i=0;i<pointlist.data.rows.length;i++){
-        var point = new BMapGL.Point(pointlist.data.rows[i].xy);
-        map.centerAndZoom(point, 15);
+        var x= pointlist.data.rows[i].x;
+        var y= pointlist.data.rows[i].y;
+        point[i] = new BMapGL.Point(x,y);
+        map.centerAndZoom(point[i], 15);
         // 创建点标记
-        var marker = new BMapGL.Marker(point);
-        map.addOverlay(marker);
+        marker[i] = new BMapGL.Marker(point[i]);
+        map.addOverlay(marker[i]);
         // 创建信息窗口
         var opts = {
             width: 200,
             height: 100,
             title: pointlist.data.rows[i].title
         };
-        var infoWindow = new BMapGL.InfoWindow('地址：'+pointlist.data.rows[i].addr, opts);
+        infoWindow[i] = new BMapGL.InfoWindow('地址：'+pointlist.data.rows[i].addr, opts);
         // 点标记添加点击事件
-        marker.addEventListener('click', function () {
-            map.openInfoWindow(infoWindow, point); // 开启信息窗口
+        marker[i].addEventListener('click', function () {
+            map.openInfoWindow(infoWindow[i], point[i]); // 开启信息窗口
         });
     }
 
